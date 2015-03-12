95f9e4fa4c293dfa0f97ccb4a22038c84937aa27
samueljmurray <samueljmurray@gmail.com>
2015-03-12 12:23:32 +0000

    initial commit

---
+++ app/SC-app-content-components.module.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name SC-app-content-components
+ * @description
+ *
+ * Provides the app with the ability to display content components content and features
+ */
+angular
+  .module('SC-app-content-components');
