a13e9bd1ac3e3fa309c29451b14e8731c0add925
samueljmurray <samueljmurray@gmail.com>
2015-03-16 13:11:15 +0000

    added content component directives into app

--- app/SC-app-content-components.directives.js
+++ app/SC-app-content-components.directives.js
@@ -1,35 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name SC-app-content-components
- * @description
- *
- * Provides the app with the ability to display content components content and features
- */
-angular
-  .module('SC-app-content-components', []);;'use strict';
-
-/**
- * @ngdoc controller
- * @name SC-app-content-components.controller:HeadingCtrl
- * @controller
- *
- * @description
- * Defines the state and behaviour of the $scope for the HeadingView state
- */
-
-angular.module('SC-app-content-components')
-  .controller('HeadingCtrl', ["$scope", function($scope) {
-
-    if ($scope.paragraph.bundle === 'subheading') {
-
-      $scope.paragraph.field_subheading  = ('<h' + $scope.paragraph.field_subheading_level + '>' + $scope.paragraph.field_subheading + '</h' + $scope.paragraph.field_subheading_level + '>');
-
-    }
-
-  }]);;'use strict';
-
 angular.module('SC-app-content-components')
   /**
    * @ngdoc directive
@@ -40,7 +10,7 @@ angular.module('SC-app-content-components')
    * Renders event list component using it's relevant template
    *
    */
-  .directive('eventList', ["$http", "$compile", function($http, $compile) {
+  .directive('eventList', function($http, $compile) {
     return {
       restrict: 'A',
       scope: true,
@@ -57,7 +27,7 @@ angular.module('SC-app-content-components')
         };
       }
     };
-  }])
+  })
   /**
    * @ngdoc directive
    * @name wowApp.directive:personList
@@ -67,7 +37,7 @@ angular.module('SC-app-content-components')
    * Renders person list component using it's relevant template
    *
    */
-  .directive('personList', ["$http", "$compile", function($http, $compile) {
+  .directive('personList', function($http, $compile) {
     return {
       restrict: 'A',
       scope: true,
@@ -84,7 +54,7 @@ angular.module('SC-app-content-components')
         };
       }
     };
-  }])
+  })
   /**
    * @ngdoc directive
    * @name wowApp.directive:pageList
@@ -94,7 +64,7 @@ angular.module('SC-app-content-components')
    * Renders content page list component using it's relevant template
    *
    */
-  .directive('pageList', ["$http", "$compile", function($http, $compile) {
+  .directive('pageList', function($http, $compile) {
     return {
       restrict: 'A',
       scope: true,
@@ -111,7 +81,7 @@ angular.module('SC-app-content-components')
         };
       }
     };
-  }])
+  })
   /**
    * @ngdoc directive
    * @name wowApp.directive:youtubePromo
@@ -121,7 +91,7 @@ angular.module('SC-app-content-components')
    * Renders youtube embed component using youtube promo view template
    *
    */
-  .directive('youtubePromo', ["$http", "$compile", "$window", function($http, $compile, $window) {
+  .directive('youtubePromo', function($http, $compile, $window) {
     return {
       restrict: 'A',
       scope: true,
@@ -201,7 +171,7 @@ angular.module('SC-app-content-components')
         };
       }
     };
-  }])
+  })
   /**
    * @ngdoc directive
    * @name wowApp.directive:linkList
@@ -211,7 +181,7 @@ angular.module('SC-app-content-components')
    * Renders link list component using link list view template
    *
    */
-  .directive('linkList', ["$http", "$compile", function($http, $compile) {
+  .directive('linkList', function($http, $compile) {
     return {
       restrict: 'A',
       scope: true,
@@ -228,7 +198,7 @@ angular.module('SC-app-content-components')
         };
       }
     };
-  }])
+  })
   /**
    * @ngdoc directive
    * @name wowApp.directive:htmlBlock
@@ -238,7 +208,7 @@ angular.module('SC-app-content-components')
    * Renders HTML block component using HTML block view template
    *
    */
-  .directive('htmlBlock', ["$http", "$compile", function($http, $compile) {
+  .directive('htmlBlock', function($http, $compile) {
     return {
       restrict: 'A',
       scope: true,
@@ -255,7 +225,7 @@ angular.module('SC-app-content-components')
         };
       }
     };
-  }])
+  })
   /**
    * @ngdoc directive
    * @name wowApp.directive:twitterFeed
@@ -265,7 +235,7 @@ angular.module('SC-app-content-components')
    * Renders twitter feed component using it's relevant template
    *
    */
-  .directive('twitterFeed', ["$http", "$compile", function($http, $compile) {
+  .directive('twitterFeed', function($http, $compile) {
     return {
       restrict: 'A',
       scope: true,
@@ -282,77 +252,4 @@ angular.module('SC-app-content-components')
         };
       }
     };
-  }]);;'use strict';
-
-/**
- * @ngdoc controller
- * @name SC-app-content-components.controller:TwitterFeedCtrl
- * @controller
- *
- * @description
- * Defines the state and behaviour of the $scope for the twitterFeed state
- */
-
-angular.module('SC-app-content-components')
-  .controller('TwitterFeedCtrl', ["$scope", "$rootScope", "$http", "$window", function($scope, $rootScope, $http, $window) {
-
-    // Load the tweets from the API
-    $http.get('/json/favourited-tweets/' + $rootScope.festivalId)
-      .success(function(data) {
-
-      	if (!data.errors) {
-
-      		$scope.twitterValid = true;
-      	
-	      	$scope.tweets = data;
-
-	      	angular.forEach($scope.tweets, function(tweet, i) {
-
-	      		// Format creation date/time with moment twitter format
-	      		$scope.tweets[i].created_at = $window.moment(tweet.created_at).fromNow();
-
-	      		// Begin formatting of entities
-	      		var entities = {};
-
-	      		// Hashtags
-	      		angular.forEach(tweet.entities.hashtags, function(hashtag) {
-	      			entities[hashtag.indices[0]] = '<a href="https://twitter.com/hashtag/' + hashtag.text + '">';
-	      			entities[hashtag.indices[1]] = '</a>';
-	      		});
-
-	      		// Media
-	      		angular.forEach(tweet.entities.media, function(media) {
-	      			entities[media.indices[0]] = '<a href="' + media.media_url + '">';
-	      			entities[media.indices[1]] = '</a>';
-	      		});
-
-	      		// URLs
-	      		angular.forEach(tweet.entities.urls, function(url) {
-	      			entities[url.indices[0]] = '<a href="' + url.url + '">';
-	      			entities[url.indices[1]] = '</a>';
-	      		});
-
-	      		// User mentions
-	      		angular.forEach(tweet.entities.user_mentions, function(user_mention) {
-	      			entities[user_mention.indices[0]] = '<a href="http://twitter.com/' + user_mention.screen_name + '">';
-	      			entities[user_mention.indices[1]] = '</a>';
-	      		});
-
-	      		// Apply entity markup
-	      		var delta = 0;
-	      		angular.forEach(entities, function(entity, pos) {
-
-	      			var n = parseInt(pos, 10) + delta;
-	      			$scope.tweets[i].text = $scope.tweets[i].text.substr(0, n) + entity + $scope.tweets[i].text.substr(n);
-	      			delta = delta + entity.length;
-
-	      		});
-
-	      	});
-
-		}
-
-
-      });
-
-  }]);
+  });
