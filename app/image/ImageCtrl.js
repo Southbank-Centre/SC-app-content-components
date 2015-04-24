a5f53164a06151f14a90e35cb067a5bfb6a0e4e7
samueljmurray <samueljmurray@gmail.com>
2015-04-24 09:32:08 +0100

    make call to API to get image URL

--- app/image/ImageCtrl.js
+++ app/image/ImageCtrl.js
@@ -10,7 +10,15 @@
  */
 
 angular.module('SC-app-content-components')
-  .controller('ImageCtrl', function($scope) {
+  .controller('ImageCtrl', function($scope, $http) {
+
+    // if we have a file id in the image field, get the URL of the file
+    if ($scope.contentComponent.field_image.file.id) {
+      $http.get('/json/file/' + $scope.contentComponent.field_image.file.id + '.json')
+        .success(function(file) {
+          $scope.contentComponent.field_image.file.url = file.url;
+        });
+    }
 
     $scope.image = $scope.contentComponent;
 
