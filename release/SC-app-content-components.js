079c93fd5a2e5cf16d7b1bb505b983bc3ed07a53
Dave Vernon <dave.vernon@southbankcentre.co.uk>
2015-06-22 15:40:08 +0100

    add file upload component

--- release/SC-app-content-components.js
+++ release/SC-app-content-components.js
@@ -133,6 +133,33 @@ angular.module('SC-app-content-components')
 
 /**
  * @ngdoc controller
+ * @name SC-app-content-components.controller:FileUploadCtrl
+ * @controller
+ *
+ * @description
+ * Defines the state and behaviour of the $scope for the fileUploadView state
+ */
+
+angular.module('SC-app-content-components')
+  .controller('FileUploadCtrl', ["$scope", "$http", function($scope, $http) {
+
+    angular.forEach($scope.contentComponent.field_file_upload, function(item) {
+
+      $http.get('/json/file/' + item.file.id + '.json')
+        .success(function(file) {
+          item.file.url = file.url;
+          item.file.name = file.name;
+          item.file.size = parseInt(file.size);
+        });
+
+    });
+
+    $scope.fileUpload = $scope.contentComponent;
+
+  }]);;'use strict';
+
+/**
+ * @ngdoc controller
  * @name SC-app-content-components.controller:HeadingCtrl
  * @controller
  *
